fac7fedb0bf86e943fa00db754b8586d403d9ca9
FernandoY53 <fernandoyg95@hotmail.com>
2017-02-16 14:09:57 -0600

    Create ejercicio2161.js

---
+++ ejercicio2161.js
@@ -0,0 +1,9 @@
+var miCanvas=document.getElementById("miCanvas");
+var contexto=miCanvas.getContext("2d");
+contexto.fillStyle="red";
+contexto.fillRect(30, 30, 50, 50);
+
+var miCanvas1=document.getElementById("miCanvas1");
+var contexto=miCanvas1.getContext("2d");
+contexto.fillStyle="blue";
+contexto.fillRect(30, 30, 50, 50);
