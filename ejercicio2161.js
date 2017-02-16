dbdcf28c932fd0beba7f04e241c45b34b3696178
FernandoY53 <fernandoyg95@hotmail.com>
2017-02-16 14:13:08 -0600

    Update ejercicio2161.js

--- ejercicio2161.js
+++ ejercicio2161.js
@@ -7,3 +7,8 @@ var miCanvas1=document.getElementById("miCanvas1");
 var contexto=miCanvas1.getContext("2d");
 contexto.fillStyle="blue";
 contexto.fillRect(30, 30, 50, 50);
+
+var miCanvas2=document.getElementById("miCanvas2");
+var contexto=miCanvas2.getContext("2d");
+contexto.fillStyle="yellow";
+contexto.fillRect(30, 30, 50, 50);
