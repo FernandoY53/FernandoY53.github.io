f27b52ade23ed643028a85d05f573dd303e17924
FernandoY53 <fernandoyg95@hotmail.com>
2017-02-21 13:51:35 -0600

    Update EjemploThree.js

--- EjemploThree.js
+++ EjemploThree.js
@@ -2,7 +2,7 @@ var escena = new THREE.Scene();
 
 var camara = new THREE.PerspectiveCamera();
 
-camara.position.z = 10;
+camara.position.z = 5;
 
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerHeight*.9, window.innerHeight*.9 );
