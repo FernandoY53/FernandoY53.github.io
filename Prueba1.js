baeb9773cf3dd7fb0e88d777ad3c960751ef00fd
FernandoY53 <fernandoyg95@hotmail.com>
2017-02-23 13:54:20 -0600

    Update Prueba1.js

--- Prueba1.js
+++ Prueba1.js
@@ -6,15 +6,15 @@ forma.vertices.push( new THREE.Vector3( 1,  0, -1 ) ); // Vértice 1
 forma.vertices.push( new THREE.Vector3(-1,  0, -1 ) ); // Vértice 2
 forma.vertices.push( new THREE.Vector3(-1,  0,  1 ) ); // Vértice 3
 forma.vertices.push( new THREE.Vector3( 0,  1,  0 ) ); // Vértice 4
-forma.vertices.push( new THREE.Vector3( 0,  -1,  0 ) ); // Vértice 5
+//forma.vertices.push( new THREE.Vector3( 0,  -1,  0 ) ); // Vértice 5
 
 forma.faces.push( new THREE.Face3( 3, 2, 1 ) ); // Cara 0
-forma.faces.push( new THREE.Face3( 3, 1, 0 ) ); // Cara 1
-forma.faces.push( new THREE.Face3( 3, 0, 4 ) ); // Cara 2
+//forma.faces.push( new THREE.Face3( 3, 1, 0 ) ); // Cara 1
+//forma.faces.push( new THREE.Face3( 3, 0, 4 ) ); // Cara 2
 forma.faces.push( new THREE.Face3( 0, 1, 4 ) ); // Cara 3
 forma.faces.push( new THREE.Face3( 1, 2, 4 ) ); // Cara 4
 forma.faces.push( new THREE.Face3( 2, 3, 4 ) ); // Cara 5
-forma.faces.push( new THREE.Face3( 2, 3, 4 ) ); // Cara 6
+//forma.faces.push( new THREE.Face3( 2, 3, 4 ) ); // Cara 6
 
 forma.computeBoundingSphere();
 
@@ -23,7 +23,7 @@ forma.computeFaceNormals();
 var material = new THREE.MeshNormalMaterial();
 
 var malla = new THREE.Mesh( forma, material );
-malla.rotateX(Math.PI/4);
+malla.rotateX(Math.PI);
 
 var escena = new THREE.Scene();
 escena.add( malla );
