02ec90c6e29cb6e39cfec14188b48877154b427d
FernandoY53 <fernandoyg95@hotmail.com>
2017-02-28 13:23:18 -0600

    Update Prueba1.js

--- Prueba1.js
+++ Prueba1.js
@@ -1,40 +1,4 @@
-//Se define Objeto
-var forma = new THREE.Geometry();
-
-forma.vertices.push( new THREE.Vector3( 1,  0,  1 ) ); // Vértice 0
-forma.vertices.push( new THREE.Vector3( 1,  0, -1 ) ); // Vértice 1
-forma.vertices.push( new THREE.Vector3(-1,  0, -1 ) ); // Vértice 2
-forma.vertices.push( new THREE.Vector3(-1,  0,  1 ) ); // Vértice 3
-forma.vertices.push( new THREE.Vector3( -1,  1,  1 ) ); // Vértice 4
-forma.vertices.push( new THREE.Vector3( -1,  1,  -1 ) ); // Vértice 5
-forma.vertices.push( new THREE.Vector3( 1,  1,  -1 ) ); // Vértice 6
-forma.vertices.push( new THREE.Vector3( 1,  1,  1 ) ); // Vértice 7
-
-forma.faces.push( new THREE.Face3( 3, 2, 1 ) ); // Cara 0
-forma.faces.push( new THREE.Face3( 3, 1, 0 ) ); // Cara 1
-forma.faces.push( new THREE.Face3( 3, 0, 4 ) ); // Cara 2
-forma.faces.push( new THREE.Face3( 0, 1, 4 ) ); // Cara 3
-forma.faces.push( new THREE.Face3( 1, 2, 4 ) ); // Cara 4
-forma.faces.push( new THREE.Face3( 2, 3, 4 ) ); // Cara 5
-//forma.faces.push( new THREE.Face3( 2, 3, 4 ) ); // Cara 6
-
-forma.computeBoundingSphere();
-
-forma.computeFaceNormals();
-
-var material = new THREE.MeshNormalMaterial();
-
-var malla = new THREE.Mesh( forma, material );
-malla.rotateX(Math.PI);
-
-var escena = new THREE.Scene();
-escena.add( malla );
-
-var camara = new THREE.PerspectiveCamera();
-camara.position.z = 5;
-
-var renderizador = new THREE.WebGLRenderer();
-renderizador.setSize( window.innerHeight*.95, 
-                      window.innerHeight*.95 );
-document.body.appendChild( renderizador.domElement );
-renderizador.render( escena, camara );
+var geometry = new THREE.CylinderBufferGeometry( 5, 5, 20, 32 );
+var material = new THREE.MeshBasicMaterial( {color: 0xffff00} );
+var cylinder = new THREE.Mesh( geometry, material );
+scene.add( cylinder );
