d15c432a5bd48c142f837b1bb03f8db905889304
FernandoY53 <fernandoyg95@hotmail.com>
2017-02-28 13:43:18 -0600

    Update Prueba1.js

--- Prueba1.js
+++ Prueba1.js
@@ -1,9 +1,11 @@
 var points = [];
+
 for ( var i = 0; i < 10; i ++ ) {
 	points.push( new THREE.Vector2( Math.sin( i * 0.2 ) * 10 + 5, ( i - 5 ) * 2 ) );
 }
+
 var geometry = new THREE.LatheGeometry( points );
-var material = new THREE.MeshBasicMaterial( { color: 0xffff00 } );
+var material = new THREE.MeshNormalMaterial();
 
 var malla = new THREE.Mesh( geometry, material );
 malla.rotateX(Math.PI/6);
@@ -12,7 +14,7 @@ var scene=new THREE.Scene();
 scene.add( malla);
 
 var camara = new THREE.PerspectiveCamera();
-camara.position.z = 5;
+camara.position.z = 500;
 
 var renderizador = new THREE.WebGLRenderer();
 renderizador.setSize( window.innerHeight*.95, 
