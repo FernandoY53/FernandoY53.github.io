24b20d81ed09bcb329841d6350d4bc5d82f95285
FernandoY53 <fernandoyg95@hotmail.com>
2017-02-28 13:55:27 -0600

    Update Prueba1.js

--- Prueba1.js
+++ Prueba1.js
@@ -20,4 +20,4 @@ var renderizador = new THREE.WebGLRenderer();
 renderizador.setSize( window.innerHeight*.95, 
                       window.innerHeight*.95 );
 document.body.appendChild( renderizador.domElement );
-renderizador.render( escena, camara );
+renderizador.render( scene, camara );
