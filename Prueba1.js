a35dd7a966efaa22be0d9681397659d41501029c
FernandoY53 <fernandoyg95@hotmail.com>
2017-02-28 13:24:44 -0600

    Update Prueba1.js

--- Prueba1.js
+++ Prueba1.js
@@ -2,3 +2,15 @@ var geometry = new THREE.CylinderBufferGeometry( 5, 5, 20, 32 );
 var material = new THREE.MeshBasicMaterial( {color: 0xffff00} );
 var cylinder = new THREE.Mesh( geometry, material );
 scene.add( cylinder );
+
+var escena = new THREE.Scene();
+escena.add( malla );
+
+var camara = new THREE.PerspectiveCamera();
+camara.position.z = 5;
+
+var renderizador = new THREE.WebGLRenderer();
+renderizador.setSize( window.innerHeight*.95, 
+                      window.innerHeight*.95 );
+document.body.appendChild( renderizador.domElement );
+renderizador.render( escena, camara );
