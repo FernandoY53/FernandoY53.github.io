d9804a77226d795f5546c71df892f038e9392e80
FernandoY53 <fernandoyg95@hotmail.com>
2017-03-03 17:38:21 -0600

    Update Prueba5.js

--- Prueba5.js
+++ Prueba5.js
@@ -18,7 +18,7 @@ var escena = new THREE.Scene();
 escena.add(malla);
 
 var camara = new THREE.PerspectiveCamera();
-camara.position.z = 400;
+camara.position.z = 200;
 
 var renderizador = new THREE.WebGLRenderer();
 renderizador.setSize( window.innerHeight*.95,
