3d01c203230cbf123866f89fc42e83dfecc8d4f5
FernandoY53 <fernandoyg95@hotmail.com>
2017-03-02 23:47:06 -0600

    Update Prueba3.js

--- Prueba3.js
+++ Prueba3.js
@@ -31,3 +31,4 @@ renderizador.setSize( window.innerHeight*.95,
                       window.innerHeight*.95 );
 document.body.appendChild( renderizador.domElement );
 renderizador.render( escena, camara );
+
