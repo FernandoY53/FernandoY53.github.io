37e5cf876d24a67f8914445bde5ee12b601d1fd4
FernandoY53 <fernandoyg95@hotmail.com>
2017-03-03 17:21:27 -0600

    Create Prueba5.js

---
+++ Prueba5.js
@@ -0,0 +1,25 @@
+var figura = new THREE.Shape();
+
+figura.moveTo(10, 10);
+figura.lineTo(10, 10);
+figura.lineTo(30, 10);
+figura.lineTo(35, 15);
+figura.lineTo(30, 30);
+figura.lineTo(10, 30);
+figura.lineTo(5, 15);
+
+var material = new THREE.MeshNormalMaterial();
+var malla = new THREE.Mesh( forma, material );
+malla.rotateY( Math.PI/6);
+
+var escena = new THREE.Scene();
+escena.add(malla);
+
+var camara = new THREE.PerspectiveCamera();
+camara.position.x = 500;
+
+var renderizador = new THREE.WebGLRenderer();
+renderizador.setSize( window.innerHeight*.95,
+                      window.innerHeight*.95 );
+document.body.appendChild( renderizador.domElement );
+renderizador.render( escena, camara );
