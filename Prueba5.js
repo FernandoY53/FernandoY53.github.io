4818511f1371dc129002b5ad14cd5e350592c6de
FernandoY53 <fernandoyg95@hotmail.com>
2017-03-04 21:25:39 -0600

    Update Prueba5.js

--- Prueba5.js
+++ Prueba5.js
@@ -3,7 +3,7 @@ ctx.arc(0,0,1.5,0,2*Math.PI);
 var forma1 = new THREE.ExtrudeGeometry(ctx,{amount: 20} );
 var material1 = new THREE.MeshNormalMaterial();
 var malla1 = new THREE.Mesh( forma1, material1 );
-malla1.rotateX( Math.-PI/3);
+malla1.rotateX( Math.PI/3);
 var escena1 = new THREE.Scene();
 escena1.add(malla1);
 
@@ -24,7 +24,7 @@ var forma = new THREE.ExtrudeGeometry( figura,{amount: 10} );
 
 var material = new THREE.MeshNormalMaterial();
 var malla = new THREE.Mesh( forma, material );
-malla.rotateX( Math.-PI/3);
+malla.rotateX( Math.PI/3);
 
 var escena = new THREE.Scene();
 escena.add(malla);
