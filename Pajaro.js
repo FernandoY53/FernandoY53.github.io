208deb1e1482e6dee1356fe982b8b17b914626f4
FernandoY53 <fernandoyg95@hotmail.com>
2017-03-05 16:38:22 -0600

    Update Pajaro.js

--- Pajaro.js
+++ Pajaro.js
@@ -1,20 +1,28 @@
 var figura = new THREE.Shape();
 
-figura.moveTo(10, 10);
-figura.lineTo(10, 60);    //1
-figura.lineTo(10, 40);    //2
-figura.lineTo(20, 30);    //3
-figura.lineTo(20, 20);    //4
-figura.lineTo(30, 10);    //5
-figura.lineTo(40, 10);    //6
-figura.lineTo(50, 20);    //7
-figura.lineTo(50, 30);    //8
-figura.lineTo(60, 40);    //9
-figura.lineTo(60, 60);    //10
-figura.lineTo(55, 50);    //11
-figura.lineTo(45, 70);    //12
-figura.lineTo(25, 70);    //13
-figura.lineTo(15, 50);    //14
+figura.moveTo(10, 30);
+figura.lineTo(10, 50);    //1
+figura.lineTo(20, 60);    //2
+figura.lineTo(20, 70);    //3
+figura.lineTo(30, 80);    //4
+figura.lineTo(40, 80);    //5
+figura.lineTo(50, 70);    //6
+figura.lineTo(50, 60);    //7
+figura.lineTo(60, 50);    //8
+figura.lineTo(60, 30);    //9
+figura.lineTo(55, 40);    //10
+figura.lineTo(45, 20);    //11
+figura.lineTo(42.22, 20);    //12
+figura.lineTo(42.22, 10);    //13
+figura.lineTo(37.78, 10);    //14
+figura.lineTo(37.78, 20);    //15
+figura.lineTo(32.22, 20);    //16
+figura.lineTo(32.22, 10);    //17
+figura.lineTo(27.78, 10);    //18
+figura.lineTo(27.78, 20);    //19
+figura.lineTo(25, 20);    //20
+figura.lineTo(15, 40);    //21
+
 
 var forma = new THREE.ExtrudeGeometry( figura,{amount: 10} );
 
