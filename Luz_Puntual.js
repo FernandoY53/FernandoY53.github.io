82bc007c82acff4364b85e5f45a61307e5085dd5
FernandoY53 <fernandoyg95@hotmail.com>
2017-04-18 13:48:45 -0500

    Create Luz_Puntual.js

---
+++ Luz_Puntual.js
@@ -0,0 +1,19 @@
+var iluminacion = new THREE.PointLight(0xFFFFFF);
+iluminacion.position.y = 20;
+
+var forma = new THREE.SphereGeometry(1);
+var material = new THREE.MeshLambertMaterial({color: "#00cc00"});
+var malla = new THREE.Mesh( forma, material);
+malla.position.y = 2;
+
+var base = new THREE.Mesh(  new THREE.BosGeometry(5,.1,5);
+                            new THREE.MeshLambertMaterial(
+                            {color: 0xFFFFFF}));
+                            
+var escena  = new THREE.Scene();
+escena.add(malla);
+escena.add(base);
+escena.add(iluminacion);
+
+var camara = new THREE.PerspectiveCamera();
+camara.position.z = 15;
