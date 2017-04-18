3eedf096d3479df622e7a3377e43179264ebe947
FernandoY53 <fernandoyg95@hotmail.com>
2017-04-18 13:52:39 -0500

    Update Luz_Puntual.js

--- Luz_Puntual.js
+++ Luz_Puntual.js
@@ -17,3 +17,10 @@ escena.add(iluminacion);
 
 var camara = new THREE.PerspectiveCamera();
 camara.position.z = 15;
+
+var renderizador = new THREE.WebGLRenderer();
+renderizador.setSize( window.innerHeight*.95, 
+                      window.innerHeight*.95 );
+
+document.body.appendChild( renderizador.domElement );
+renderizador.render( escena, camara );
